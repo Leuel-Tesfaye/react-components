d810bda82c26435527223d78847f76f30f3e188a
Leuel-Tesfaye <131950742+Leuel-Tesfaye@users.noreply.github.com>
2023-07-12 11:19:43 +0300

    changes in tech list and user card

--- src/index.js
+++ src/index.js
@@ -5,21 +5,46 @@ import './styles.css';
 /** creating react components */
 /** Functional Component */
 
-const component = (
-  <header>
-    <h1>header testing</h1>
-  </header>
-)
 
-const app= (
-  <div>
-    {component}
+/** react component  */
+const Header =() =>{
+  return ( <header>
+  <div className='header-wrapper'>
+  <h1>Welcome to 30 Days Of React</h1>
+    <h2>Getting Started React</h2>
+    <h3>JavaScript Library</h3>
+    <p>Prince Hope</p>
+    <small>Oct 3, 2020</small>
   </div>
-)
+</header>)
+}
 
+const UserCard = () =>{
+  return (
+    <div className='userCard'>
+    <img src='{./img}' alt='profile img'/>
+    <h2>Prince Hope</h2>
+    </div>
+  )
+}
+
+const TechList = () =>{
+    const techs = ['HTML', 'CSS', 'JavaScript']
+  const techsFormatted = techs.map((tech) => <li key={tech}>{tech}</li>)
+  return techsFormatted
+}
+
+const App = () =>{
+  return (
+    <div className='app'>
+      {<Header/>}
+      {<userCard/>}
+    </div>
+  )
+}
 const rootElement = document.getElementById('root')
 
-ReactDOM.render(app, rootElement)
+ReactDOM.render(<App/>, rootElement)
 
 
 
