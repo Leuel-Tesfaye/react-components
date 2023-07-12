ebc42a5b2e61dfd66bcd13e1a67574f7be987f20
Leuel-Tesfaye <131950742+Leuel-Tesfaye@users.noreply.github.com>
2023-07-12 10:50:50 +0300

    setting up react-components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import './styles.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+/** creating react components */
+/** Functional Component */
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const component = (
+  <header>
+    <h1>header testing</h1>
+  </header>
+)
+
+const app= (
+  <div>
+    {component}
+  </div>
+)
+
+const rootElement = document.getElementById('root')
+
+ReactDOM.render(app, rootElement)
+
+
+
+
